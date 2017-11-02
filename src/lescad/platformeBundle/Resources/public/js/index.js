64e492b5f79783f804d39602b8551aeb166681d6
Donald <donald.kouazoun@outlook.fr>
2017-11-02 02:00:03 +0100

    Modification du carousel de la page d'accueil

--- src/lescad/platformeBundle/Resources/public/js/index.js
+++ src/lescad/platformeBundle/Resources/public/js/index.js
@@ -1,17 +1,42 @@
-$(function () {
-    var count = 0;
-    $('.owl-carousel').each(function () {
-        $(this).attr('id', 'owl-demo' + count);
-        $('#owl-demo' + count).owlCarousel({
-            navigation: false,
-            slideSpeed: 200,
-            pagination: true,
-            singleItem: true,
-            autoPlay: 10000,
-            autoHeight: true
-        });
-        count++;
-    });
-});
+(function( $ ) {
 
+    //Function to animate slider captions 
+	function doAnimations( elems ) {
+		//Cache the animationend event in a variable
+		var animEndEv = 'webkitAnimationEnd animationend';
+		
+		elems.each(function () {
+			var $this = $(this),
+				$animationType = $this.data('animation');
+			$this.addClass($animationType).one(animEndEv, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+	
+	//Variables on page load 
+	var $myCarousel = $('#carousel-example-generic'),
+		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+		
+	//Initialize carousel 
+	$myCarousel.carousel();
+	
+	//Animate captions in first slide on page load 
+	doAnimations($firstAnimatingElems);
+	
+	//Pause carousel  
+	$myCarousel.carousel('pause');
+	
+	
+	//Other slides to be animated on carousel slide event 
+	$myCarousel.on('slide.bs.carousel', function (e) {
+		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+		doAnimations($animatingElems);
+	});  
+    $('#carousel-example-generic').carousel({
+        interval:10000,
+        pause: "false"
+    });
+	
+})(jQuery);	
 
